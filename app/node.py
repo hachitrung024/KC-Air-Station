64fa1756127e689c23fd10062a5f0c73208eb99a
Tien CSE <tien.nguyen11102004@hcmut.edu.vn>
2025-04-17 16:12:59 +0700

    catch json data uart

--- app/node.py
+++ app/node.py
@@ -1,6 +1,13 @@
+import json
 async def node_task(uart_rx_queue, lora_tx_queue):
     while True:
         uart_rx_data = await uart_rx_queue.get()
         # need some processing ?
-        print(uart_rx_data)
-        await lora_tx_queue.put(uart_rx_data) # directly foward
+        print(f"Receive from core 2 : {uart_rx_data}")
+        try:
+            data_part = uart_rx_data[13:]
+            final_data = json.loads(data_part)
+            print(f"Parse json sucess {final_data}")
+        except:
+            continue
+        await lora_tx_queue.put(final_data) # directly foward

a78f8771619c0c290f2a375edc0d625268611717
Trung Ha <hachitrung024@gmail.com>
2025-04-18 14:35:25 +0100

    map device and sensor value

--- app/gateway.py
+++ app/gateway.py
@@ -1,6 +1,16 @@
 import asyncio
 import json
+import configparser
 
+config = configparser.ConfigParser()
+config.optionxform = str
+config.read("config/settings.ini")
+
+station_map = dict(config["station_mapping"])
+sensor_map = dict(config["sensor_mapping"])
+print("Mapping:")
+print(station_map)
+print(sensor_map)
 def is_valid_json(text):
     try:
         json.loads(text)
@@ -10,9 +20,35 @@ def is_valid_json(text):
 
 async def gateway_task(lora_rx_queue, mqtt_publish_queue):
     while True:
+        await asyncio.sleep(0.01)
         data = await lora_rx_queue.get()
         if isinstance(data, bytes):
-            data = data.decode('utf-8')
-        if is_valid_json(data):
-            await mqtt_publish_queue.put(data)
-        await asyncio.sleep(0.1)
+            data = data.decode("utf-8")
+        if not is_valid_json(data):
+            continue
+
+        raw_json = json.loads(data)
+
+        if not isinstance(raw_json, dict) or len(raw_json) != 1:
+            continue
+
+        station_id = list(raw_json.keys())[0]
+        readings = raw_json[station_id]
+
+        if station_id not in station_map or not isinstance(readings, list):
+            continue
+
+        mapped_station = station_map[station_id]
+        raw_reading = readings[0]
+
+        # sensor key mapping
+        mapped_reading = {}
+        for k, v in raw_reading.items():
+            if k in sensor_map:
+                mapped_reading[sensor_map[k]] = v
+
+        if not mapped_reading:
+            continue
+
+        final_payload = json.dumps({mapped_station: [mapped_reading]})
+        await mqtt_publish_queue.put(final_payload)
--- app/mqtt_publisher.py
+++ app/mqtt_publisher.py
@@ -11,13 +11,19 @@ MQTT_PORT = 1883
 mqtt_client = mqtt.Client()
 def init_mqtt():
     mqtt_client.username_pw_set(MQTT_TOKEN)
-
+    mqtt_client.on_message = on_message
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
     mqtt_client.loop_start()
-
+    mqtt_client.subscribe("v1/gateway/attributes")    
+    mqtt_client.subscribe("v1/gateway/rpc")    
 async def mqtt_publisher(mqtt_publish_queue):
     while True:
         payload = await mqtt_publish_queue.get()
         mqtt_client.publish("v1/gateway/telemetry", payload)
         print(payload)
         await asyncio.sleep(1)
+
+def on_message(client, userdata, msg):
+    topic = msg.topic
+    payload = msg.payload.decode('utf-8')
+    print(f"MQTT message received | Topic: {topic} | Payload: {payload}")
